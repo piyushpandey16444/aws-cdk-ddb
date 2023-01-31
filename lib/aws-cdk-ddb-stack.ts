bc607f1642d3a43ba3a62a7648364e67cc8e425c
Piyush Pandey <piyushpandey16444@gmail.com>
2023-01-31 22:48:01 +0530

    adding lambda function

--- lib/aws-cdk-ddb-stack.ts
+++ lib/aws-cdk-ddb-stack.ts
@@ -1,13 +1,17 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class AwsCdkDdbStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const fn = new lambda.Function(this, 'getStudentList', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    });
 
     const table = new dynamodb.Table(this, 'Student', {
       partitionKey: { name: 'student_id', type: dynamodb.AttributeType.STRING },
