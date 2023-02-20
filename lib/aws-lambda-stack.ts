f2614ef0b2ffd6c03fa71821e84c770a9025b0f3
Piyush Pandey <piyushpandey16444@gmail.com>
2023-02-20 14:18:41 +0530

    updated API Gateway

--- lib/aws-lambda-stack.ts
+++ lib/aws-lambda-stack.ts
@@ -1,29 +1,37 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigw from '@aws-cdk/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 
-export class AwsCdkLambdaStack extends Stack {
+export class AwsCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const lambdaPath = 'src/lambda/';
-    const lambdaName = 'lambda_handler';
+    const lambdaName = 'get-products';
 
     const fn = new lambda.Function(this, 'sa360-'+ lambdaName + '-dev', {
-      functionName: lambdaName + '-dev',
+      functionName:  'sa360-'+ lambdaName + '-dev',
       runtime: lambda.Runtime.PYTHON_3_7,
-      handler: 'get-products.lambda_handler',
+      handler: 'get-products.get_products',
       code: lambda.Code.fromAsset(lambdaPath)
-    //   role: myRole, // user-provided role
     });
 
-    // const myRole = new iam.Role(this, 'My Role', {
-    //   assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    const api = new apigw.RestApi(this, 'sa360', {
+    restApiName: 'SA360 API',
+    description: 'This is my API Gateway from CDK',
+    });
+
+    const productDetail = api.root.addResource('product-detail');
+
+    productDetail.addMethod('GET', fn);
+
+    // deployment dynamodb table
+    // const table = new dynamodb.Table(this, 'Student', {
+    //     partitionKey: { name: 'student_id', type: dynamodb.AttributeType.STRING },
     // });
-        
-    // myRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
 
 }
 }
